70c6bff1bc95b8054f2ca2763ac2af15056a8870
宇 <31683427+674197141@users.noreply.github.com>
2020-05-12 20:28:26 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
     url="https://github.com/674197141/flask_pydantic_easy",
     author="刘宇",
     author_email="674197141@qq.com",
-
+long_description=README,
+    long_description_content_type="text/markdown",
     packages=["flask_pydantic_easy"],
     include_package_data=True,
     platforms="any",
